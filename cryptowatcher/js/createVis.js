7e34b7a057c68798e0e068e154f92917a975ded5
James R. Ingram II <jamesingram2@outlook.com>
2022-02-16 00:45:23 -0500

    Add files via upload

---
+++ cryptowatcher/js/createVis.js
@@ -0,0 +1,15 @@
+export function tableOnClick() {
+   let visEl = document.getElementById("graphsDiv");
+   visEl.innerHTML += `<img src="./img/sample_graph.png">`;
+   let table = document.querySelector('table');
+   for (let i = 0; i < table.rows.length; i++) {
+      table.rows[i].addEventListener('click', function() {
+         let coinKey = this.cells[1].innerText;
+         let headingName = this.cells[2].innerText;
+         visEl.innerHTML = `
+            <h3><i class="bi bi-graph-up"></i> ${headingName} Historical Data</h3>
+            <img src="./img/${coinKey.toLowerCase()}.png">
+         `;
+      })
+   }
+}
